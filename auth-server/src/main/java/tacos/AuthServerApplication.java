40d8c55b22a8c9aeea0144a9ee9eac21f9071be3
toptenov <toptenov@gmail.com>
2022-07-08 17:30:30 +0300

    Add Authorization Server

---
+++ auth-server/src/main/java/tacos/AuthServerApplication.java
@@ -0,0 +1,28 @@
+package tacos;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import tacos.authorization.users.User;
+import tacos.authorization.users.UserRepository;
+
+@SpringBootApplication
+public class AuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServerApplication.class, args);
+	}
+
+	@Bean
+	public ApplicationRunner dataLoader(UserRepository repo, PasswordEncoder encoder) {
+		return args -> {
+			repo.save(new User("habuma", encoder.encode("password"), "ROLE_ADMIN"));
+			repo.save(new User("tacochef", encoder.encode("password"), "ROLE_ADMIN"));
+			repo.save(new User("Please", encoder.encode("Please"), "ROLE_ADMIN"));
+		};
+	}
+
+}
--- taco-cloud/src/main/java/tacos/web/api/IngredientController.java
+++ taco-cloud/src/main/java/tacos/web/api/IngredientController.java
@@ -1,7 +1,7 @@
 package tacos.web.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,7 +22,6 @@ public class IngredientController {
 
     private IngredientRepository ingredientRepo;
 
-    @Autowired
     public IngredientController(IngredientRepository ingredientRepo) {
         this.ingredientRepo = ingredientRepo;
     }
@@ -33,12 +32,14 @@ public class IngredientController {
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('USER')")
     @ResponseStatus(HttpStatus.CREATED)
     public Ingredient saveIngredient(@RequestBody Ingredient ingredient) {
         return ingredientRepo.save(ingredient);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('USER')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteIngredient(@PathVariable("id") String ingredientId) {
         ingredientRepo.deleteById(ingredientId);
